27167e3b58087affdf3ca865ab503acb9c57d01d
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-05-31 16:41:27 -0300

    feat(add): add NotFound.js tests, req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente <NotFound.js />', () => {
+  it('Testa se a página contém um h2 com o texto Page requested not found', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/page-not-found');
+    });
+
+    const h2 = screen.getByText(/Page requested not found/i);
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Testa se a página mostra uma imagem na tela', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/page-not-found');
+    });
+
+    const img = screen.getByRole('img');
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
