a9c451d445e76ed43a0a489e9dfa319100e56198
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-05-31 15:58:02 -0300

    feat(add): add About.js tests, req 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,42 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente <About.js />', () => {
+  it('Testa se a página contem um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(aboutLink);
+
+    const aboutPokedex = screen.getByText(/About Pokédex/i);
+    expect(aboutPokedex).toBeInTheDocument();
+
+    const h2 = screen.getByRole('heading', { level: 2 });
+    expect(h2).toBeInTheDocument();
+    expect(h2).toHaveTextContent('About Pokédex');
+  });
+
+  it('Testa se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(aboutLink);
+
+    const firstP = screen.getByText(/This application simulates a Pokédex/i);
+    expect(firstP).toBeInTheDocument();
+
+    const secondP = screen.getByText(/One can filter Pokémon by type/i);
+    expect(secondP).toBeInTheDocument();
+  });
+
+  it('Testa se a página contém uma imágem de uma Pokédex', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(aboutLink);
+
+    const img = screen.getByRole('img');
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
